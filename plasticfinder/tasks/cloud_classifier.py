bff636a317d763b25be80e9f14142d458de0fdf3
Pierre Tessier <pierre.borie.tessier@gmail.com>
2022-02-04 11:23:43 -0500

    fixed cloud classifier: multi_temporal => mono_temporal mask

--- plasticfinder/tasks/cloud_classifier.py
+++ plasticfinder/tasks/cloud_classifier.py
@@ -7,10 +7,12 @@ def get_cloud_classifier_task():
 
        Configures an instance of the EOTask s2_pixel_cloud_detector and AddCloudMaskTask
     """
-    
+
     cloud_detection_task = CloudMaskTask(processing_resolution='120m',
                                          data_feature="BANDS-S2-L1C",
                                          is_data_feature="IS_DATA",
+                                         mono_features=(None, 'CLM_S2C'),
+                                         mask_feature=None,
                                          average_over=16,
                                          dilation_size=8)
     return cloud_detection_task
